fe3d6b695ee1d7f7cc6cb220735510bbb310ae8d
Nam Nguyen <jhinn.114@gmail.com>
2025-11-05 19:35:17 +0700

    Remove i18n config for static export

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ const nextConfig = {
     images: {
         unoptimized: true,
     },
-    i18n: {
-        locales: ['en', 'vi'],
-        defaultLocale: 'en',
-    },
     // Optional: Add basePath if your site is not at the root of the domain
     // basePath: '/your-repo-name',
 };
