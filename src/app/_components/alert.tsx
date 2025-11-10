0fb67cef5f3673a6d1fa52f0b1ebc55c42969d04
karify98 <nnnam048@gmail.com>
2025-11-10 21:29:09 +0700

    remote text unescesary

--- src/app/_components/alert.tsx
+++ src/app/_components/alert.tsx
@@ -7,6 +7,8 @@ type Props = {
 };
 
 const Alert = ({ preview }: Props) => {
+  if (!preview) return null;
+
   return (
     <div
       className={cn("border-b dark:bg-slate-800", {
@@ -16,29 +18,16 @@ const Alert = ({ preview }: Props) => {
     >
       <Container>
         <div className="py-2 text-center text-sm">
-          {preview ? (
-            <>
-              This page is a preview.{" "}
-              <a
-                href="/api/exit-preview"
-                className="underline hover:text-teal-300 duration-200 transition-colors"
-              >
-                Click here
-              </a>{" "}
-              to exit preview mode.
-            </>
-          ) : (
-            <>
-              The source code for this blog is{" "}
-              <a
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-                className="underline hover:text-blue-600 duration-200 transition-colors"
-              >
-                available on GitHub
-              </a>
-              .
-            </>
-          )}
+          <>
+            This page is a preview.{" "}
+            <a
+              href="/api/exit-preview"
+              className="underline hover:text-teal-300 duration-200 transition-colors"
+            >
+              Click here
+            </a>{" "}
+            to exit preview mode.
+          </>
         </div>
       </Container>
     </div>
